3ee7669b05859456269ecea70ad414cbeeab5653
Ramy selim <padelfinder.top@gmail.com>
2023-07-31 15:03:21 +0300

    fix margin

--- lib/app/modules/main_tabs/views/main_tabs_view.dart
+++ lib/app/modules/main_tabs/views/main_tabs_view.dart
@@ -21,65 +21,51 @@ class MainTabsView extends GetView<MainTabsController> {
     return GetBuilder<MainTabsController>(
         builder: (_) => Scaffold(
               body: _getPage(),
-              bottomNavigationBar: Container(
-                padding: EdgeInsets.only(top: 0),
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                        color: Color(0xFF1E1E22).withOpacity(0.06),
-                        offset: Offset(0, -2),
-                        spreadRadius: 0,
-                        blurRadius: 2)
-                  ],
-                ),
-                child: BottomNavigationBar(
-                  showSelectedLabels: true,
-                  showUnselectedLabels: false,
-                  elevation: 0,
-                  backgroundColor: HexColor("#141415"),
-                  onTap: controller.onTap,
-                  unselectedItemColor: Colors.white.withOpacity(0.25),
-                  selectedFontSize: 20,
-                  unselectedFontSize: 0,
-                  selectedItemColor: Colors.white,
-                  currentIndex:
-                      controller.pageIndex < 0 ? 0 : controller.pageIndex,
-                  type: BottomNavigationBarType.fixed,
-                  items: [
-                    BottomNavigationBarItem(
-                        icon: Icon(
-                          Remix.dashboard_fill,
-                          size: 25,
-                        ),
-                        label: 'Garage'.toUpperCase()),
-                    BottomNavigationBarItem(
-                        icon: Icon(
-                          Remix.compass_3_line,
-                          size: 25,
-                        ),
-                        label: 'Feed'.toUpperCase()),
-                    BottomNavigationBarItem(
-                        icon: Icon(
-                          Remix.notification_2_line,
-                          size: 25,
-                        ),
-                        label: 'Alerts'.toUpperCase()),
-                    BottomNavigationBarItem(
-                        icon: AppProfileAvatar(
-                          user: controller.settingsService.authModel?.userModel,
-                          size: 25,
-                        ),
-                        label: 'Profile'.toUpperCase()),
-                    // BottomNavigationBarItem(
-                    //     icon: Icon(
-                    //       Remix.group_line,
-                    //       size: 25,
-                    //     ),
-                    //     label: 'Leaders'.toUpperCase()),
-                  ],
-                ),
+              bottomNavigationBar: BottomNavigationBar(
+                showSelectedLabels: true,
+                showUnselectedLabels: false,
+                elevation: 0,
+                backgroundColor: HexColor("#141415"),
+                onTap: controller.onTap,
+                unselectedItemColor: Colors.white.withOpacity(0.25),
+                selectedFontSize: 20,
+                unselectedFontSize: 0,
+                selectedItemColor: Colors.white,
+                currentIndex:
+                    controller.pageIndex < 0 ? 0 : controller.pageIndex,
+                type: BottomNavigationBarType.fixed,
+                items: [
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        Remix.dashboard_fill,
+                        size: 25,
+                      ),
+                      label: 'Garage'.toUpperCase()),
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        Remix.compass_3_line,
+                        size: 25,
+                      ),
+                      label: 'Feed'.toUpperCase()),
+                  BottomNavigationBarItem(
+                      icon: Icon(
+                        Remix.notification_2_line,
+                        size: 25,
+                      ),
+                      label: 'Alerts'.toUpperCase()),
+                  BottomNavigationBarItem(
+                      icon: AppProfileAvatar(
+                        user: controller.settingsService.authModel?.userModel,
+                        size: 25,
+                      ),
+                      label: 'Profile'.toUpperCase()),
+                  // BottomNavigationBarItem(
+                  //     icon: Icon(
+                  //       Remix.group_line,
+                  //       size: 25,
+                  //     ),
+                  //     label: 'Leaders'.toUpperCase()),
+                ],
               ),
             ));
   }
