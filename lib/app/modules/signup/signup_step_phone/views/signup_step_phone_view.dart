7549e95ed0370f935356d5442ff8c0d7cc60c121
macbook <myemail@example.com>
2024-01-03 14:44:08 +0500

    social logins and settings page

--- lib/app/modules/signup/signup_step_phone/views/signup_step_phone_view.dart
+++ lib/app/modules/signup/signup_step_phone/views/signup_step_phone_view.dart
@@ -31,162 +31,160 @@ class SignupStepPhoneView extends GetView<SignupStepPhoneController> {
               horizontal: 20,
               vertical: 60,
             ),
-            child: Expanded(
-              child: SingleChildScrollView(
-                child: Form(
-                  key: empSignUpFormKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      OuterAppBar(
-                        title: '',
-                      ),
-                      SizedBox(
-                        height: 100,
-                      ),
-                      Text700(
-                        text: 'Welcome, Lets setup your account',
-                        textAlign: TextAlign.center,
-                        fontSize: 22,
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
+            child: SingleChildScrollView(
+              child: Form(
+                key: empSignUpFormKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    OuterAppBar(
+                      title: '',
+                    ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                    Text700(
+                      text: 'Welcome, Lets setup your account',
+                      textAlign: TextAlign.center,
+                      fontSize: 22,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
 
-                      AppTextField(
-                        hintText: 'Username',
-                        labelText: 'Username',
-                        onChanged: (val) {
-                          controller.username = val;
-                        },
-                      ),
-                      SizedBox(
-                        height: 80,
-                      ),
-                      Container(
-                        height: 55,
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        width: double.infinity,
-                        child: AppButtonField(
-                          text: 'Continue',
-                          onPressed: () {
-                            FocusScope.of(context).unfocus();
-                            if (controller.username.isEmpty) {
-                              controller.dialogService
-                                  .showError('Please Enter a UserName');
-                            } else if (controller.username.isNotEmpty) {
-                              if (RegExp(r'\s').hasMatch(controller.username)) {
-                                controller.dialogService.showError(
-                                    'Username can not include spaces');
-                              } else {
-                                controller.next();
-                              }
+                    AppTextField(
+                      hintText: 'Username',
+                      labelText: 'Username',
+                      onChanged: (val) {
+                        controller.username = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Container(
+                      height: 55,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      width: double.infinity,
+                      child: AppButtonField(
+                        text: 'Continue',
+                        onPressed: () {
+                          FocusScope.of(context).unfocus();
+                          if (controller.username.isEmpty) {
+                            controller.dialogService
+                                .showError('Please Enter a UserName');
+                          } else if (controller.username.isNotEmpty) {
+                            if (RegExp(r'\s').hasMatch(controller.username)) {
+                              controller.dialogService.showError(
+                                  'Username can not include spaces');
+                            } else {
+                              controller.next();
                             }
+                          }
+                        },
+                        primary: AppColors.primary,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    // Container(
+                    //   child: Row(
+                    //     mainAxisAlignment: MainAxisAlignment.center,
+                    //     crossAxisAlignment: CrossAxisAlignment.center,
+                    //     children: [
+                    //       Container(
+                    //         color: Colors.white,
+                    //         height: 2,
+                    //         width: MediaQuery.of(context).size.width / 2.8,
+                    //       ),
+                    //       Text(
+                    //         " OR ".toUpperCase(),
+                    //         style: TextStyle(
+                    //             fontSize: 18,
+                    //             color: Colors.white,
+                    //             fontWeight: FontWeight.w600,
+                    //             fontStyle: FontStyle.italic),
+                    //       ),
+                    //       Container(
+                    //         color: Colors.white,
+                    //         height: 2,
+                    //         width: MediaQuery.of(context).size.width / 2.8,
+                    //       ),
+                    //     ],
+                    //   ),
+                    // ),
+                    // SizedBox(
+                    //   height: 10,
+                    // ),
+                    // Container(
+                    //   height: 55,
+                    //   margin:
+                    //       EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    //   width: double.infinity,
+                    //   child: AppButtonField(
+                    //     prefix: Image.asset(
+                    //       'assets/images/google.png',
+                    //       height: 24,
+                    //       width: 24,
+                    //     ),
+                    //     text: 'Continue with Google',
+                    //     onPressed: () async {
+                    //      // await controller.signInWithGoogle();
+                    //       //Get.toNamed(Routes.MAIN_TABS);
+                    //     },
+                    //     textColor: Colors.black.withOpacity(0.54),
+                    //     primary: Colors.white,
+                    //     haveBorder: false,
+                    //   ),
+                    // ),
+                    // Container(
+                    //   height: 55,
+                    //   margin:
+                    //       EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    //   width: double.infinity,
+                    //   child: AppButtonField(
+                    //     prefix: Image.asset(
+                    //       'assets/images/facebook.png',
+                    //       height: 24,
+                    //       width: 24,
+                    //     ),
+                    //     text: 'Continue with Facebook',
+                    //     onPressed: () async {
+                    //      // await controller.signInWithFacebook();
+                    //       //Get.toNamed(Routes.MAIN_TABS);
+                    //     },
+                    //     primary: Color(0xfff1877F2),
+                    //     haveBorder: false,
+                    //   ),
+                    // ),
+                    SizedBox(
+                      height: 100,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text400(
+                            textAlign: TextAlign.center,
+                            fontSize: 14,
+                            text: "By continuing you are agreeing with our "),
+                        GestureDetector(
+                          onTap: () {
+                            launchUrl(_url);
                           },
-                          primary: AppColors.primary,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      // Container(
-                      //   child: Row(
-                      //     mainAxisAlignment: MainAxisAlignment.center,
-                      //     crossAxisAlignment: CrossAxisAlignment.center,
-                      //     children: [
-                      //       Container(
-                      //         color: Colors.white,
-                      //         height: 2,
-                      //         width: MediaQuery.of(context).size.width / 2.8,
-                      //       ),
-                      //       Text(
-                      //         " OR ".toUpperCase(),
-                      //         style: TextStyle(
-                      //             fontSize: 18,
-                      //             color: Colors.white,
-                      //             fontWeight: FontWeight.w600,
-                      //             fontStyle: FontStyle.italic),
-                      //       ),
-                      //       Container(
-                      //         color: Colors.white,
-                      //         height: 2,
-                      //         width: MediaQuery.of(context).size.width / 2.8,
-                      //       ),
-                      //     ],
-                      //   ),
-                      // ),
-                      // SizedBox(
-                      //   height: 10,
-                      // ),
-                      // Container(
-                      //   height: 55,
-                      //   margin:
-                      //       EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                      //   width: double.infinity,
-                      //   child: AppButtonField(
-                      //     prefix: Image.asset(
-                      //       'assets/images/google.png',
-                      //       height: 24,
-                      //       width: 24,
-                      //     ),
-                      //     text: 'Continue with Google',
-                      //     onPressed: () async {
-                      //      // await controller.signInWithGoogle();
-                      //       //Get.toNamed(Routes.MAIN_TABS);
-                      //     },
-                      //     textColor: Colors.black.withOpacity(0.54),
-                      //     primary: Colors.white,
-                      //     haveBorder: false,
-                      //   ),
-                      // ),
-                      // Container(
-                      //   height: 55,
-                      //   margin:
-                      //       EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                      //   width: double.infinity,
-                      //   child: AppButtonField(
-                      //     prefix: Image.asset(
-                      //       'assets/images/facebook.png',
-                      //       height: 24,
-                      //       width: 24,
-                      //     ),
-                      //     text: 'Continue with Facebook',
-                      //     onPressed: () async {
-                      //      // await controller.signInWithFacebook();
-                      //       //Get.toNamed(Routes.MAIN_TABS);
-                      //     },
-                      //     primary: Color(0xfff1877F2),
-                      //     haveBorder: false,
-                      //   ),
-                      // ),
-                      SizedBox(
-                        height: 100,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text400(
+                          child: Text400(
                               textAlign: TextAlign.center,
+                              decoration: TextDecoration.underline,
                               fontSize: 14,
-                              text: "By continuing you are agreeing with our "),
-                          GestureDetector(
-                            onTap: () {
-                              launchUrl(_url);
-                            },
-                            child: Text400(
-                                textAlign: TextAlign.center,
-                                decoration: TextDecoration.underline,
-                                fontSize: 14,
-                                color: AppColors.primary,
-                                text: " privacy policy"),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                              color: AppColors.primary,
+                              text: " privacy policy"),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
